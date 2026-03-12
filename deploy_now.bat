d19e413078277d656babc9a8d7a530ef0300c187
Fikrat Gadimov <49558682+Fikret98@users.noreply.github.com>
2026-03-12 09:27:59 +0400

    1

---
+++ deploy_now.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Updating Supabase Secrets...
+call npx -y supabase secrets set --project-ref canoruljgackpmziotel VAPID_PUBLIC_KEY="BFsCmKN5EyRWAs6XPIxmMwEH3BOYAjECqRn2LZgPYJMqigPUmjxeO2UTNkBUDQ9anOrvqjEFMU78E6ZvnTi9NlE" VAPID_PRIVATE_KEY="IduX3jUkWRhz2ADp2FHwK7iDWqiAbTjSL0sInHapvg"
+echo Deploying Edge Function...
+call npx -y supabase functions deploy send-push --project-ref canoruljgackpmziotel
+echo DONE!
+pause
